cc2bb42780162d2de23d9a927770d5ffe4e4d943
River <foxnezz@gmail.com>
2022-12-04 19:11:02 +0400

    [ADD] add score parsing and calculation

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+import json
 
 
-print("hello world123")
+def main():
+    animes = get_animes()
+    scores = get_scores(animes)
+    print_scores(scores)
+
+
+def get_animes():
+    raw = open('table', 'r').read()
+    return json.loads(raw)
+
+
+def get_scores(animes):
+    scores = {}
+    for anime in animes:
+        score = anime['score']
+
+        if score not in scores.keys():
+            scores[score] = 1
+        else:
+            scores[score] += 1
+
+    return scores
+
+
+def print_scores(scores):
+    # MAX_LENGTH = 20
+    print()
+    print(scores)
+    print()
+
+
+main()
